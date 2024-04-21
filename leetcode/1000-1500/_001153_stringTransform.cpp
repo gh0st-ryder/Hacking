a33fde14662ec128db96bc8bc8da1007511f6a5d
Sourabh S Joshi <11589484+gh0st-ryder@users.noreply.github.com>
2024-04-21 14:51:24 -0700

    [Hard] Interesting string problem.

---
+++ leetcode/1000-1500/_001153_stringTransform.cpp
@@ -0,0 +1,48 @@
+class Solution {
+
+vector<int> conv_positional(const string& str) {
+    unordered_map<char, int> freqs;
+    for (char ch : str) {
+        if (freqs.find(ch) == freqs.end()) freqs[ch] = 1; else freqs[ch]++;
+    }
+    vector<int> ret(str.size(), 0);
+    for (int i=0; i<str.size(); i++) {ret[i] = freqs[str[i]];}
+    return ret;
+}
+
+// Impossible if one src char needs to turn into 2 dst chars.
+// Src char can change, but the freq (of the changed) cannot be greater than the freq in the dest for that char.
+bool is_impossible(const vector<int>& src, const vector<int>& dst) {
+    for (int i=0; i<src.size(); i++) {
+        if (src[i] > dst[i]) return true;
+    }
+    return false;  // may be possible
+}
+
+bool same_positional_diff_str(const string& str1, const string& str2, const vector<int>& pos1, const vector<int>& pos2) {
+    bool same_pos = true;
+    for (int i=0; i<pos1.size(); i++) {
+        if (pos1[i] != pos2[i]) {same_pos = false; break;}
+    }
+    return (same_pos && str1 != str2);
+}
+
+public:
+    bool canConvert(string str1, string str2) {
+        if (str1.size() != str2.size()) return false;
+        if (str1 == str2) return true;
+        vector<int> pos_str1 = conv_positional(str1);
+        vector<int> pos_str2 = conv_positional(str2);
+        if (is_impossible(pos_str1, pos_str2)) return false;        
+
+        // Find letters in dest, but not in src.
+        std::map<char, int> src_chars, dst_chars;
+        for (char ch : str1) {src_chars[ch]++;}
+        for (char ch : str2) {dst_chars[ch]++;}
+        vector<pair<char, int>> sdchars;
+        std::set_difference(dst_chars.begin(), dst_chars.end(), src_chars.begin(), src_chars.end(), std::back_inserter(sdchars));
+        if (sdchars.size() > 1) return true;
+        if (dst_chars.size() < 26) return true;  // if not all chars are used, we can convert a src char to a temporarily unused char name
+        return false;        
+    }
+};
